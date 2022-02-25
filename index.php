e8221ce9c5f3b0c5ea4a63dc1517a32e641d6255
Princess-Bermudez <98816541+Princess-Bermudez@users.noreply.github.com>
2022-02-25 14:19:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
    <title> CLE </title> 
    <link rel="icon" href="9d g22.png" type="image/icon type">
  </head> 
-<body background= "webpg bg.jpg">
+<body background= "cle bg.png">
 <center> 
  <br> 
  <br> 
  <br> 
  <br> 
- <h1><font color="white" face="Calibri"> Digital Creative Journal </h1></font> 
+ <h1><font face="Calibri"> Digital Creative Journal </h1></font> 
  <br> 
  <br> 
  <a href="Third Performace Task.docx"> 
