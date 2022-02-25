e81cab6372c89d20b6211f0b5914bfd670572db7
Princess-Bermudez <98816541+Princess-Bermudez@users.noreply.github.com>
2022-02-25 13:36:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,12 @@
    <link rel="icon" href="9d g22.png" type="image/icon type">
  </head> 
 <body background= "webpg bg.jpg">
+<center> 
+ <h2><font color="white" face="Calibri"> Digital Creative Journal </h2></font> 
+ <br> 
+ <br> 
+ <a href="Third Performace Task.docx"> 
+  Click here to see "Third Performance Task" Document
+ </a> 
+ </html> 
+ </body> 
